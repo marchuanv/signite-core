e5b21710e350b15f814270d42baab571425faac7
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-05-12 13:52:31 +0200

    working

--- index.js
+++ index.js
@@ -16,20 +16,21 @@ server.on('request', (request, response) => {
             const indexHtmlStr = fs.readFileSync(path.join(__dirname, 'index.html'));
             response.statusMessage = 'Success';
             response.statusCode = 200;
-            return response.end(indexHtmlStr);
-        }
-        const resourcePath = path.join(__dirname, request.url);
-        if (fs.existsSync(resourcePath)) {
-            response.setHeader('content-type', 'application/javascript');
-            const indexHtmlStr = fs.readFileSync(resourcePath);
-            response.statusMessage = 'Success';
-            response.statusCode = 200;
             response.end(indexHtmlStr);
         } else {
-            response.setHeader('content-type', 'text/plain');
-            response.statusMessage = 'Not Found';
-            response.statusCode = 404;
-            response.end('404 Not Found');
+            const resourcePath = path.join(__dirname, request.url);
+            if (fs.existsSync(resourcePath)) {
+                response.setHeader('content-type', 'application/javascript');
+                const indexHtmlStr = fs.readFileSync(resourcePath);
+                response.statusMessage = 'Success';
+                response.statusCode = 200;
+                response.end(indexHtmlStr);
+            } else {
+                response.setHeader('content-type', 'text/plain');
+                response.statusMessage = 'Not Found';
+                response.statusCode = 404;
+                response.end('404 Not Found');
+            }
         }
     });
 });
