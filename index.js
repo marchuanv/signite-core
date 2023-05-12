fab113b3b243a0b6635f8c4c2636c4ade27b9eff
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-05-12 12:13:50 +0200

    more setup

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 const server = http.createServer();
 server.on('request', (request, response) => {
     let body = [];
     request.on('data', (chunk) => {
         body.push(chunk);
     }).on('end', () => {
-        body = Buffer.concat(body).toString();
-        console.log('request received.', body);
-        response.end('hello world');
+        const bodyStr = Buffer.concat(body).toString();
+        body = [];
+        console.log('request received: ', bodyStr);
+        console.log('request path: ', request.url);
+        if (request.url === '/') {
+            response.setHeader('content-type', 'text/html');
+            const indexHtmlStr = fs.readFileSync(path.join(__dirname, 'index.html'));
+            response.statusMessage = 'Success';
+            response.statusCode = 200;
+            return response.end(indexHtmlStr);
+        }
+        const resourcePath = path.join(__dirname, request.url);
+        if (fs.existsSync(resourcePath)) {
+            response.setHeader('content-type', 'application/javascript');
+            const indexHtmlStr = fs.readFileSync(resourcePath);
+            response.statusMessage = 'Success';
+            response.statusCode = 200;
+            response.end(indexHtmlStr);
+        } else {
+            response.setHeader('content-type', 'text/plain');
+            response.statusMessage = 'Not Found';
+            response.statusCode = 404;
+            response.end('404 Not Found');
+        }
     });
 });
 server.listen(3000);
